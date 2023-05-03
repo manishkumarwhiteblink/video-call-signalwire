5c71bb00d61137cfa23a49ebf9011109776ee7ed
manishkumarwhiteblink <126445286+manishkumarwhiteblink@users.noreply.github.com>
2023-05-03 12:58:25 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", async () => {
+  const stream = await SignalWire.WebRTC.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  const video = document.getElementById("video");
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  console.table(stream.getVideoTracks());
+});
