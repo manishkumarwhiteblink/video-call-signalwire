94a16ccf07b44a3cb341665e36302776f1eb4aac
manishkumarwhiteblink <126445286+manishkumarwhiteblink@users.noreply.github.com>
2023-05-03 15:12:26 +0530

    Add button to request permission

--- script.js
+++ script.js
@@ -1,12 +1,15 @@
-document.addEventListener("DOMContentLoaded", async () => {
-  const stream = await SignalWire.WebRTC.getUserMedia({
-    audio: true,
-    video: true,
+document.addEventListener("DOMContentLoaded", () => {
+  const allowBtn = document.getElementById("allowBtn");
+  allowBtn.addEventListener("click", async () => {
+    const stream = await SignalWire.WebRTC.getUserMedia({
+      audio: true,
+      video: true,
+    });
+    const video = document.getElementById("video");
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+      video.play();
+    });
+    console.table(stream.getVideoTracks());
   });
-  const video = document.getElementById("video");
-  video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-  });
-  console.table(stream.getVideoTracks());
 });
