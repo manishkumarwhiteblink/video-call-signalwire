2ded4536a5e97f5f9dc17646c4ef689c23d40da5
manishkumarwhiteblink <126445286+manishkumarwhiteblink@users.noreply.github.com>
2023-05-04 15:10:59 +0530

    Try to open permission dialog without button on ios

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const allowBtn = document.getElementById("allowBtn");
-  allowBtn.addEventListener("click", async () => {
-    const stream = await SignalWire.WebRTC.getUserMedia({
-      audio: true,
-      video: true,
-    });
-    const video = document.getElementById("video");
-    video.srcObject = stream;
-    video.addEventListener("loadedmetadata", () => {
-      video.play();
-    });
-    console.table(stream.getVideoTracks());
+document.addEventListener("DOMContentLoaded", async () => {
+  const stream = await SignalWire.WebRTC.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  const video = document.getElementById("video");
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
   });
 });
